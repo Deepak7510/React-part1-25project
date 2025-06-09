db7a1f2939c3b71c221a01502e20926492c4ff7d
Deepak7510 <deepakkumaryadav75100@gmail.com>
2025-06-10 02:17:06 +0530

    first commit

---
+++ src/components/accordian/index.jsx
@@ -0,0 +1,16 @@
+function AccordianApp() {
+  return (
+    <div className="flex justify-center py-10">
+      <div className="w-[500px]">
+        <div className="flex justify-center">
+          <button className="text-white font-medium px-4 py-2 bg-green-500 rounded-2xl">
+            Enable Multi Selection
+          </button>
+        </div>
+        <div>{}</div>
+      </div>
+    </div>
+  );
+}
+
+export default AccordianApp;
